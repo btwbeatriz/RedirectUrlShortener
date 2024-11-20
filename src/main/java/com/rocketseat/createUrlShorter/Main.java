8cfbcc53c27dcfa5b6ebb09a53daa16e5c31c880
btwbeatriz <beatriz.ffreitas@hotmail.com>
2024-11-20 17:23:39 -0300

    configurando s3 e fazendo validações

--- src/main/java/com/rocketseat/createUrlShorter/Main.java
+++ src/main/java/com/rocketseat/createUrlShorter/Main.java
@@ -2,19 +2,63 @@ package com.rocketseat.createUrlShorter;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
-public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper =  new ObjectMapper();
 
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
         String pathParameters = (String) input.get("rawPath");
         String shortUtlCode = pathParameters.replace("/", "");
 
         if (shortUtlCode == null || shortUtlCode.isEmpty()) {
             throw new IllegalArgumentException("Input inválido!");
         }
-        return null;
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("url-shortener-storage-beatriz")
+                .key(shortUtlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        }catch (Exception e){
+            throw new RuntimeException("Error ao recuperar dados do S3: " + e.getMessage(), e);
+        }
+
+        UrlData urlData;
+
+        try {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        }catch (Exception e){
+            throw new RuntimeException("Erro ao converter os dados da url" + e.getMessage(), e);
+        }
+
+        Long currentTimeSeconds = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (urlData.getExpirationTime() < currentTimeSeconds){
+            response.put("statusCode", 410);
+            response.put("body", "Esta url expirou!");
+            return response;
+        }
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        return response;
     }
 }
